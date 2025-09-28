336e59ec9bf76ea16f71f14901051e5799975bd2
Carly Hessler <2931350+Chessler@users.noreply.github.com>
2025-09-27 20:20:24 -0400

    Initial commit of website, let's see how it goes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello, World!",
+        "excerpt":"This is a test post to the website to be removed or updated later!  ","categories": [],
+        "tags": [],
+        "url": "/blog/hello/",
+        "teaser": null
+      }]
