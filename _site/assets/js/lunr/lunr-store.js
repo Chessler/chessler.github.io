13e7a00dad0a6d94ce62e9ca1bf82f73eed7b667
Carly Hessler <2931350+Chessler@users.noreply.github.com>
2025-09-29 21:59:27 -0400

    Adding go plugin post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/hello/",
         "teaser": null
+      },{
+        "title": "Closed-Source Deliveries with Go Plugins",
+        "excerpt":"Let’s say your customer wants a partial closed source delivery. Source in one internal repository, API definition, shared object, and loader in another for a customer - just like C++! Go is, like, C++ plusplus so it should work too, right? Ehhhh, yes… but not without effort to figure out...","categories": [],
+        "tags": [],
+        "url": "/goclosed/",
+        "teaser": null
       }]
